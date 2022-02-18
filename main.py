647db9ff89f548b01aab1119af49ac7baa4c3af9
arahman63 <Arifur.Rahman63@myhunter.cuny.edu>
2022-02-17 23:05:41 -0500

    Updated changes

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+author:@Capstone group 
+"""
+import requests
+
+#it's a good idea to refer to the api link when using the requests library
+#to put things quotes, hold shift + ""
+
+
+
+r = requests.get("https://api.github.com/user",auth=("user","pass"))
+
+
+#you can define a set/object with ()
+
+#Pandas & NUMPY
